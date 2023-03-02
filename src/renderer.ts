c2613c75964b8b2fb9c659faa8a4ae4270fffb5b
inventivetalent <mail@inventivetalent.org>
2023-03-02 21:08:58 +0100

    move some stuff

--- src/renderer.ts
+++ src/renderer.ts
@@ -13,6 +13,7 @@ import tinycolor from "tinycolor2";
 import RGBA = tinycolor.ColorFormats.RGBA;
 import cv from "@techstark/opencv-js"
 import * as stringSimilarity from 'string-similarity';
+import {cleanupText, getNameTransform, getRole, parseNumber, tmpImg} from "./util";
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
@@ -292,22 +293,6 @@ async function takeVideoSnapshot(): Promise<string> {
     return url;
 }
 
-function tmpImg(src: string): Promise<HTMLImageElement> {
-    const img = document.createElement('img');
-    img.id = 'tmpimg-' + (Math.random() + 1).toString(36).substring(2);
-    img.crossOrigin = 'anonymous';
-    document.body.appendChild(img);
-    const promise = new Promise<HTMLImageElement>((resolve) => {
-        img.onload = function () {
-            resolve(img);
-        };
-    })
-    img.src = src;
-    setTimeout(() => {
-        img.remove();
-    }, 1000)
-    return promise;
-}
 
 ipcRenderer.on('setSource', async (event, sourceId) => {
     console.log("source", sourceId)
@@ -452,90 +437,14 @@ canvas.addEventListener('mousemove', e => {
     document.getElementById('positionInfo').textContent = `${Math.round(x)} ${Math.round(y)}`
 })
 
-function cleanupText(txt: string) {
-    return txt.replace('\n', '').trim();
-}
 
-function parseNumber(txt: string): number {
-    if (!txt) return 0;
-    txt = txt.replace(',', '');
-    txt = txt.replace('.', '');
-    txt = txt.replace('o', '0');
-    txt = txt.replace('O', '0');
-    let parsed = parseInt(txt.trim());
-    if (isNaN(parsed)) {
-        parsed = 0;
-    }
-    return parsed;
-}
 
 let ocrRunning = false
 
 
-let nameTransform: cv.Mat;
-
-function getNameTransform(): cv.Mat {
-    if (nameTransform) {
-        return nameTransform;
-    }
-    const pts1 = cv.matFromArray(4, 1, cv.CV_32FC2,
-        [
-            5, 0,
-            95, 0,
-            0, 25,
-            90, 25
-        ]
-    )
-    const pts2 = cv.matFromArray(4, 1, cv.CV_32FC2,
-        [
-            -5, 0,
-            95, 0,
-            -3, 25,
-            97, 25
-        ]
-    )
-    return nameTransform = cv.getPerspectiveTransform(pts1, pts2)
-}
-
-function getRole(jmp: Jimp): string {
-    console.time('getRole')
-
-    jmp = jmp.clone().contrast(0.2);
 
 
-    const topLeft = Jimp.intToRGBA(jmp.getPixelColor(6, 22));
-    const topRight = Jimp.intToRGBA(jmp.getPixelColor(20, 22));
-    const bottomLeft = Jimp.intToRGBA(jmp.getPixelColor(6, 38));
-    const bottomRight = Jimp.intToRGBA(jmp.getPixelColor(20, 38));
 
-    function isWhite(clr: RGBA) {
-        return clr.r > 230 && clr.g > 230 && clr.b > 230;
-    }
-
-    function isBg(clr: RGBA) {
-        return !isWhite(clr);
-    }
-
-    console.timeEnd('getRole');
-
-    // console.log(topLeft);
-    // console.log(topRight);
-    // console.log(bottomLeft);
-    // console.log(bottomRight);
-
-    if (isWhite(topLeft) && isWhite(topRight)) { // tank or dps
-        if (isWhite(bottomLeft) && isWhite(bottomRight)) {
-            // console.log("dps")
-            return 'dps';
-        }
-        // console.log("tank")
-        return 'tank';
-    }
-    // console.log("support")
-    return 'support';
-
-    // return tankCheck ? 'tank' : supportCheck ? 'support' : dpsCheck ? 'dps' :  'unknown';
-}
 
 function updatePreview() {
     if (ocrRunning) {
---
+++ src/util.ts
@@ -0,0 +1,102 @@
+import cv from "@techstark/opencv-js";
+import Jimp from "jimp";
+import RGBA = tinycolor.ColorFormats.RGBA;
+
+export function tmpImg(src: string): Promise<HTMLImageElement> {
+    const img = document.createElement('img');
+    img.id = 'tmpimg-' + (Math.random() + 1).toString(36).substring(2);
+    img.crossOrigin = 'anonymous';
+    document.body.appendChild(img);
+    const promise = new Promise<HTMLImageElement>((resolve) => {
+        img.onload = function () {
+            resolve(img);
+        };
+    })
+    img.src = src;
+    setTimeout(() => {
+        img.remove();
+    }, 1000)
+    return promise;
+}
+
+export function cleanupText(txt: string) {
+    return txt.replace('\n', '').trim();
+}
+
+export function parseNumber(txt: string): number {
+    if (!txt) return 0;
+    txt = txt.replace(',', '');
+    txt = txt.replace('.', '');
+    txt = txt.replace('o', '0');
+    txt = txt.replace('O', '0');
+    let parsed = parseInt(txt.trim());
+    if (isNaN(parsed)) {
+        parsed = 0;
+    }
+    return parsed;
+}
+
+let nameTransform: cv.Mat;
+
+export function getNameTransform(): cv.Mat {
+    if (nameTransform) {
+        return nameTransform;
+    }
+    const pts1 = cv.matFromArray(4, 1, cv.CV_32FC2,
+        [
+            5, 0,
+            95, 0,
+            0, 25,
+            90, 25
+        ]
+    )
+    const pts2 = cv.matFromArray(4, 1, cv.CV_32FC2,
+        [
+            -5, 0,
+            95, 0,
+            -3, 25,
+            97, 25
+        ]
+    )
+    return nameTransform = cv.getPerspectiveTransform(pts1, pts2)
+}
+
+export function getRole(jmp: Jimp): string {
+    console.time('getRole')
+
+    jmp = jmp.clone().contrast(0.2);
+
+
+    const topLeft = Jimp.intToRGBA(jmp.getPixelColor(6, 22));
+    const topRight = Jimp.intToRGBA(jmp.getPixelColor(20, 22));
+    const bottomLeft = Jimp.intToRGBA(jmp.getPixelColor(6, 38));
+    const bottomRight = Jimp.intToRGBA(jmp.getPixelColor(20, 38));
+
+    function isWhite(clr: RGBA) {
+        return clr.r > 230 && clr.g > 230 && clr.b > 230;
+    }
+
+    function isBg(clr: RGBA) {
+        return !isWhite(clr);
+    }
+
+    console.timeEnd('getRole');
+
+    // console.log(topLeft);
+    // console.log(topRight);
+    // console.log(bottomLeft);
+    // console.log(bottomRight);
+
+    if (isWhite(topLeft) && isWhite(topRight)) { // tank or dps
+        if (isWhite(bottomLeft) && isWhite(bottomRight)) {
+            // console.log("dps")
+            return 'dps';
+        }
+        // console.log("tank")
+        return 'tank';
+    }
+    // console.log("support")
+    return 'support';
+
+    // return tankCheck ? 'tank' : supportCheck ? 'support' : dpsCheck ? 'dps' :  'unknown';
+}
