6af31dfc3ce0e3659cd65056176ff49a5e3fe81d
inventivetalent <mail@inventivetalent.org>
2023-03-03 12:01:12 +0100

    .

---
+++ calver.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+CURR=$(json -i -f package.json version)
+CURR1=${CURR:1:-1}
+echo $CURR1
+BUMPED=$(calver inc $CURR1 --format yy.mm.dd.minor --levels calendar.minor)
+echo $BUMPED
+json -I -f package.json -e "this.version = \"$BUMPED\""
